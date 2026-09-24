75d2fdb408630f26255381ea5cc04839538ad884
bkohoutovab30-pixel <b.kohoutova.b30@gmail.com>
2026-09-24 12:07:31 +0200

    Update rostliny-data.js

--- rostliny-data.js
+++ rostliny-data.js
@@ -1,307 +1,35 @@
-/*
-  ========================================
-  ZLATÁ UDICE 2026 – ROSTLINY
-  ========================================
-
-  Tento soubor obsahuje společná data
-  pro:
-
-  - Učení
-  - Procvičování
-  - Co mi nejde
-
-  Fotografie doplníme později.
-*/
-
-
-const plants = [
-
   {
-    card: "1",
-    name: "Blatouch bahenní",
+    card: "",
+    name: "Tužebník jilmový",
     image: ""
   },
 
   {
-    card: "20",
-    name: "Bříza bradavičnatá",
+    card: "",
+    name: "Lilek potměchuť",
     image: ""
   },
 
   {
-    card: "49",
-    name: "Bublinatka obecná",
+    card: "",
+    name: "Stolístek klasnatý",
     image: ""
   },
 
   {
-    card: "11",
-    name: "Devětsil lékařský",
+    card: "",
+    name: "Řezan pilolistý",
     image: ""
   },
 
   {
-    card: "21",
-    name: "Dub",
+    card: "",
+    name: "Plavín štítnatý",
     image: ""
   },
 
   {
-    card: "41",
-    name: "Ďáblík bahenní",
-    image: ""
-  },
-
-  {
-    card: "12",
-    name: "Jaterník podléška",
-    image: ""
-  },
-
-  {
-    card: "4",
-    name: "Kontryhel obecný",
-    image: ""
-  },
-
-  {
-    card: "3",
-    name: "Kopřiva dvoudomá",
-    image: ""
-  },
-
-  {
-    card: "8",
-    name: "Kosatec žlutý",
-    image: ""
-  },
-
-  {
-    card: "7",
-    name: "Kostival lékařský",
-    image: ""
-  },
-
-  {
-    card: "40",
-    name: "Kotvice plovoucí",
-    image: ""
-  },
-
-  {
-    card: "46",
-    name: "Kyprej vrbice",
-    image: ""
-  },
-
-  {
-    card: "23",
-    name: "Lakušník vzplývavý",
-    image: ""
-  },
-
-  {
-    card: "25",
-    name: "Leknín bílý",
-    image: ""
-  },
-
-  {
-    card: "13",
-    name: "Lopuch pavučinatý",
-    image: ""
-  },
-
-  {
-    card: "47",
-    name: "Netýkavka malokvětá",
-    image: ""
-  },
-
-  {
-    card: "44",
-    name: "Okřehek menší",
-    image: ""
-  },
-
-  {
-    card: "18",
-    name: "Olše lepkavá",
-    image: ""
-  },
-
-  {
-    card: "37",
-    name: "Orobinec širokolistý",
-    image: ""
-  },
-
-  {
-    card: "38",
-    name: "Orobinec úzkolistý",
-    image: ""
-  },
-
-  {
-    card: "2",
-    name: "Orsej jarní",
-    image: ""
-  },
-
-  {
-    card: "30",
-    name: "Ostřice",
-    image: ""
-  },
-
-  {
-    card: "6",
-    name: "Pomněnka bahenní",
-    image: ""
-  },
-
-  {
-    card: "45",
-    name: "Přeslička bahenní",
-    image: ""
-  },
-
-  {
-    card: "35",
-    name: "Puškvorec obecný",
-    image: ""
-  },
-
-  {
-    card: "36",
-    name: "Rákos obecný",
-    image: ""
-  },
-
-  {
-    card: "26",
-    name: "Rdesno obojživelné",
-    image: ""
-  },
-
-  {
-    card: "32",
-    name: "Rdest kadeřavý",
-    image: ""
-  },
-
-  {
-    card: "31",
-    name: "Rdest plovoucí",
-    image: ""
-  },
-
-  {
-    card: "9",
-    name: "Rosnatka okrouhlolistá",
-    image: ""
-  },
-
-  {
-    card: "22",
-    name: "Růžkatec ponořený",
-    image: ""
-  },
-
-  {
-    card: "33",
-    name: "Sítina klubkatá",
-    image: ""
-  },
-
-  {
-    card: "43",
-    name: "Skřípinec jezerní",
-    image: ""
-  },
-
-  {
-    card: "24",
-    name: "Stulík žlutý",
-    image: ""
-  },
-
-  {
-    card: "16",
-    name: "Svízel přítula",
-    image: ""
-  },
-
-  {
-    card: "28",
-    name: "Šípatka střelolistá",
-    image: ""
-  },
-
-  {
-    card: "19",
-    name: "Topol",
-    image: ""
-  },
-
-  {
-    card: "14",
-    name: "Upolín evropský",
-    image: ""
-  },
-
-  {
-    card: "5",
-    name: "Vachta trojlistá",
-    image: ""
-  },
-
-  {
-    card: "15",
-    name: "Vlaštovičník větší",
-    image: ""
-  },
-
-  {
-    card: "48",
-    name: "Voďanka žabí",
-    image: ""
-  },
-
-  {
-    card: "29",
-    name: "Vodní mor kanadský",
-    image: ""
-  },
-
-  {
-    card: "17",
-    name: "Vrba",
-    image: ""
-  },
-
-  {
-    card: "10",
-    name: "Vrbina penízkovitá",
-    image: ""
-  },
-
-  {
-    card: "34",
-    name: "Zblochan vodní",
-    image: ""
-  },
-
-  {
-    card: "39",
-    name: "Zevar jednoduchý",
-    image: ""
-  },
-
-  {
-    card: "27",
-    name: "Žabník jitrocelový",
+    card: "",
+    name: "Šmel okoličnatý",
     image: ""
   }
-
-];
