9c0f65baf71292299b0c9361e5c6a4892fdf6bd9
bkohoutovab30-pixel <b.kohoutova.b30@gmail.com>
2026-09-24 12:34:36 +0200

    Create zivocichove-data.js

---
+++ zivocichove-data.js
@@ -0,0 +1,413 @@
+/*
+  ========================================
+  ZLATÁ UDICE 2026 – ŽIVOČICHOVÉ
+  ========================================
+
+  Společný seznam živočichů pro:
+
+  - Učení
+  - Procvičování
+  - Co mi nejde
+
+  Fotografie doplníme později.
+*/
+
+
+const animals = [
+
+  /* POLYPOVCI */
+
+  {
+    card: "1",
+    name: "Nezmar zelený",
+    image: ""
+  },
+
+
+  /* PLOŠTĚNCI */
+
+  {
+    card: "2",
+    name: "Ploštěnka potoční",
+    image: ""
+  },
+
+
+  /* KROUŽKOVCI */
+
+  {
+    card: "4",
+    name: "Chobotnatka rybí",
+    image: ""
+  },
+
+  {
+    card: "3",
+    name: "Pijavka koňská",
+    image: ""
+  },
+
+  {
+    card: "5",
+    name: "Nítěnka větší",
+    image: ""
+  },
+
+
+  /* MĚKKÝŠI */
+
+  {
+    card: "9",
+    name: "Bahenka živorodá",
+    image: ""
+  },
+
+  {
+    card: "10",
+    name: "Okružák ploský",
+    image: ""
+  },
+
+  {
+    card: "11",
+    name: "Perlorodka říční",
+    image: ""
+  },
+
+  {
+    card: "8",
+    name: "Plovatka bahenní",
+    image: ""
+  },
+
+  {
+    card: "6",
+    name: "Škeble rybničná",
+    image: ""
+  },
+
+  {
+    card: "7",
+    name: "Velevrub malířský",
+    image: ""
+  },
+
+
+  /* KORÝŠI */
+
+  {
+    card: "13",
+    name: "Beruška vodní",
+    image: ""
+  },
+
+  {
+    card: "15",
+    name: "Blešivec obecný",
+    image: ""
+  },
+
+  {
+    card: "14",
+    name: "Kapřivec plochý",
+    image: ""
+  },
+
+  {
+    card: "12",
+    name: "Rak říční",
+    image: ""
+  },
+
+
+  /* PAVOUKOVCI */
+
+  {
+    card: "16",
+    name: "Vodouch stříbřitý",
+    image: ""
+  },
+
+
+  /* HMYZ */
+
+  {
+    card: "23",
+    name: "Bruslařka obecná",
+    image: ""
+  },
+
+  {
+    card: "26",
+    name: "Chrostík velký",
+    image: ""
+  },
+
+  {
+    card: "18",
+    name: "Jehlanka válcovitá",
+    image: ""
+  },
+
+  {
+    card: "25",
+    name: "Jepice obecná",
+    image: ""
+  },
+
+  {
+    card: "24",
+    name: "Komár pisklavý",
+    image: ""
+  },
+
+  {
+    card: "30",
+    name: "Pakomár kouřový",
+    image: ""
+  },
+
+  {
+    card: "29",
+    name: "Potápník vroubený",
+    image: ""
+  },
+
+  {
+    card: "28",
+    name: "Šídlo modré",
+    image: ""
+  },
+
+  {
+    card: "27",
+    name: "Vážka ploská",
+    image: ""
+  },
+
+  {
+    card: "21",
+    name: "Vírník obecný",
+    image: ""
+  },
+
+  {
+    card: "22",
+    name: "Vodoměrka štíhlá",
+    image: ""
+  },
+
+  {
+    card: "20",
+    name: "Vodomil černý",
+    image: ""
+  },
+
+  {
+    card: "19",
+    name: "Znakoplavka obecná",
+    image: ""
+  },
+
+
+  /* OBOJŽIVELNÍCI */
+
+  {
+    card: "37",
+    name: "Čolek obecný",
+    image: ""
+  },
+
+  {
+    card: "35",
+    name: "Kuňka obecná",
+    image: ""
+  },
+
+  {
+    card: "36",
+    name: "Mlok skvrnitý",
+    image: ""
+  },
+
+  {
+    card: "34",
+    name: "Ropucha obecná",
+    image: ""
+  },
+
+  {
+    card: "31",
+    name: "Rosnička zelená",
+    image: ""
+  },
+
+  {
+    card: "32",
+    name: "Skokan hnědý",
+    image: ""
+  },
+
+  {
+    card: "33",
+    name: "Skokan zelený",
+    image: ""
+  },
+
+
+  /* PLAZI */
+
+  {
+    card: "38",
+    name: "Užovka obojková",
+    image: ""
+  },
+
+  {
+    card: "39",
+    name: "Zmije obecná",
+    image: ""
+  },
+
+
+  /* PTÁCI */
+
+  {
+    card: "41",
+    name: "Břehule říční",
+    image: ""
+  },
+
+  {
+    card: "50",
+    name: "Bukač velký",
+    image: ""
+  },
+
+  {
+    card: "53",
+    name: "Čáp bílý",
+    image: ""
+  },
+
+  {
+    card: "49",
+    name: "Čejka chocholatá",
+    image: ""
+  },
+
+  {
+    card: "48",
+    name: "Čírka obecná",
+    image: ""
+  },
+
+  {
+    card: "46",
+    name: "Husa velká",
+    image: ""
+  },
+
+  {
+    card: "47",
+    name: "Kachna divoká",
+    image: ""
+  },
+
+  {
+    card: "55",
+    name: "Kormorán velký",
+    image: ""
+  },
+
+  {
+    card: "45",
+    name: "Labuť velká",
+    image: ""
+  },
+
+  {
+    card: "40",
+    name: "Ledňáček říční",
+    image: ""
+  },
+
+  {
+    card: "52",
+    name: "Lyska černá",
+    image: ""
+  },
+
+  {
+    card: "56",
+    name: "Polák velký",
+    image: ""
+  },
+
+  {
+    card: "51",
+    name: "Potápka roháč",
+    image: ""
+  },
+
+  {
+    card: "44",
+    name: "Racek chechtavý",
+    image: ""
+  },
+
+  {
+    card: "43",
+    name: "Rákosník obecný",
+    image: ""
+  },
+
+  {
+    card: "42",
+    name: "Skorec vodní",
+    image: ""
+  },
+
+  {
+    card: "54",
+    name: "Volavka popelavá",
+    image: ""
+  },
+
+
+  /* SAVCI */
+
+  {
+    card: "60",
+    name: "Bobr evropský",
+    image: ""
+  },
+
+  {
+    card: "59",
+    name: "Hryzec vodní",
+    image: ""
+  },
+
+  {
+    card: "58",
+    name: "Ondatra pižmová",
+    image: ""
+  },
+
+  {
+    card: "57",
+    name: "Vydra říční",
+    image: ""
+  },
+
+
+  /* KRUHOÚSTÍ */
+
+  {
+    card: "",
+    name: "Mihule potoční",
+    image: ""
+  }
+
+];
